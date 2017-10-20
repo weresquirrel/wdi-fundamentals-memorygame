0babeceec3a458505d75b626a7f0bf4b2a3dc72b
Krisztina Sztupakne Molnar <vermokuss@gmail.com>
2017-10-20 12:59:01 +0100

    created game logic

---
+++ js/main.js
@@ -0,0 +1,15 @@
+console.log("Up and running!");
+
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
+
+
+
+
+
+
