45e5d484cea7c4b51bbdc1cefdf709f110a40d2e
Krisztina Sztupakne Molnar <vermokuss@gmail.com>
2017-10-25 15:56:51 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 console.log("Up and running!");
 
-var cardOne = "king";
+/*var cardOne = "king";
 var cardTwo = "king";
 var cardThree = "queen";
-var cardFour = "queen";
+var cardFour = "queen";*/
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []; 
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardsInPlay);
 
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else if (cardsInPlay.length === 2) {
+	alert("Sorry, try again.");
+} else {
+	alert("Click one more card, please.");
+}
 
 
 
