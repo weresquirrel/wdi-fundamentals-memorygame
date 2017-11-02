a03fb9eaca4678924aa9c3dbf9f7910ee1bd6a00
Krisztina Sztupakne Molnar <vermokuss@gmail.com>
2017-11-02 12:48:24 +0000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
-console.log("Up and running!");
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",		
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",		
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",		
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",		
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 
-var cards = ["queen", "queen", "king", "king"];
+
 var cardsInPlay = []; 
 
-/*
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay);
-*/
 
 var checkForMatch = function() {
 	/*if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
@@ -29,13 +41,16 @@ var checkForMatch = function() {
 };
 
 var flipCard = function(cardId) {	
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);	
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 };
 
+
 flipCard(0);
-flipCard(1);
+flipCard(2);
 
 
 
